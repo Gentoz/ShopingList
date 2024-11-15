0e077c2019da9325aa4586cfd839394d88a9ff45
Sergey Kuznetsov <sk300102@yandex.ru>
2024-11-15 14:43:29 +0300

    data_add

---
+++ app/src/main/java/com/github/gentoz/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.github.gentoz.data
+
+import com.github.gentoz.domain.ShopItem
+import com.github.gentoz.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId } ?:
+        throw RuntimeException("Element with id $shopItemId is not found")
+    }
+
+}
