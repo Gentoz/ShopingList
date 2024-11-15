ac295d75e7c531aca844418ebc95a7a4943edd71
Sergey Kuznetsov <sk300102@yandex.ru>
2024-11-15 14:03:42 +0300

    Initial commit

---
+++ app/src/main/java/com/github/gentoz/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.github.gentoz
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.github.gentoz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+
+
+    }
+}
